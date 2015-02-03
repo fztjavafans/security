e6013d533f92ed87fcafbcabbcc5012dab3ac4e8
fztjavafans <fztjavafans@163.com>
2015-02-03 15:13:12 +0800

    Update security.js

--- www/security.js
+++ www/security.js
@@ -1,35 +1,33 @@
-cordova.define("com.app.plugin.security", function(require, exports, module) {
-	var exec = require("cordova/exec");
-	
-	module.exports = {
-		encrypt: function(txt) {
-			exec(
-			function() {
-				//success call back
-				alert("success");
-			},
-			function() {
-				//fail call back
-				alert("fail");
-			}, "Security", "encrypt", [txt]);
+var exec = require("cordova/exec");
+
+module.exports = {
+	encrypt: function(txt) {
+		exec(
+		function() {
+			//success call back
+			alert("success");
 		},
-		decrypt: function(txt) {
-			exec(
-			function() {
-				//success call back
-			},
-			function() {
-				//fail call back
-			}, "Security", "decrypt", [txt]);
+		function() {
+			//fail call back
+			alert("fail");
+		}, "Security", "encrypt", [txt]);
+	},
+	decrypt: function(txt) {
+		exec(
+		function() {
+			//success call back
 		},
-		md5: function(txt) {
-			exec(
-			function() {
-				//success call back
-			},
-			function() {
-				//fail call back
-			}, "Security", "md5", [txt]);
-		}
-	};
-});
+		function() {
+			//fail call back
+		}, "Security", "decrypt", [txt]);
+	},
+	md5: function(txt) {
+		exec(
+		function() {
+			//success call back
+		},
+		function() {
+			//fail call back
+		}, "Security", "md5", [txt]);
+	}
+};
