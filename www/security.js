b27ed3820b758d384ff6d418da37204992270ab4
fico <fztjavafans@163.com>
2015-02-04 12:14:17 +0800

    Update security.js

--- www/security.js
+++ www/security.js
@@ -1,22 +1,13 @@
 
 var exec = require("cordova/exec");
 module.exports = {
-	encrypt: function(txt) {
-		exec(
-		function(signtxt) {
-			return signtxt[0];
-		}, null, "Security", "encrypt", [txt]);
+	encrypt: function(success, error, txt) {
+		exec(success, error, "Security", "encrypt", [txt]);
 	},
-	decrypt: function(txt) {
-		exec(
-		function(signtxt) {
-			return signtxt[0];
-		}, null, "Security", "decrypt", [txt]);
+	decrypt: function(success, error, txt) {
+		exec(success, error, "Security", "decrypt", [txt]);
 	},
-	md5: function(txt) {
-		exec(
-		function(signtxt) {
-			return signtxt[0];
-		}, null, "Security", "md5", [txt]);
+	md5: function(success, error, txt) {
+		exec(success, error, "Security", "md5", [txt]);
 	}
 };
